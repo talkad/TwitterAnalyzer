92fd8df0f102c3b8e350a59b0131f785080875e5
talkad <talkad@post.bgu.ac.il>
2022-05-29 14:31:41 +0300

    integration adjustments

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
         tweets_placeholder = st.empty()
         with st.spinner('Loading...'):
             # load the data
-            scraper.fetch_data(start_date, end_date)
+            scraper.fetch_data(search_keywords, start_date, end_date)
             # pass stock symbol, start date & end date
             sell, buy, hold = scraper.keywords_counter()
             investment_counts = [sell, buy, hold]
@@ -50,42 +50,49 @@ def main():
             labels = ['Buy', 'Sell', 'Hold']
             colors = ['#2EFF2E', '#FF5C5C', '#C5C5C5']
             explode = (0.025, 0.025, 0.025)
-            wedges, texts, autotexts = ax.pie(investment_counts, colors=colors, autopct='%1.1f%%',
-                                              startangle=90, pctdistance=0.75, explode=explode)
-            for autotext in autotexts:
-                autotext.set_weight('bold')
 
-            # Draw circle
-            centre_circle = plt.Circle((0, 0), 0.65, fc='white')
-            fig = plt.gcf()
-            fig.gca().add_artist(centre_circle)
-            ax.annotate(sum(investment_counts), xy=(0, 0), fontsize=50, weight='bold', ha="center")
-            ax.annotate("Reviews", xy=(0, -0.25), fontsize=25, ha="center")
+            if sum(investment_counts) != 0:
+                wedges, texts, autotexts = ax.pie(investment_counts, colors=colors, autopct='%1.1f%%',
+                                                  startangle=90, pctdistance=0.75, explode=explode)
 
-            # Draw bottom legend
-            labels_counts = [str(count) + " " + label for count, label in zip(list(investment_counts), labels)]
-            ax.legend(wedges, labels_counts, loc="lower center", ncol=3,
-                      prop={'weight': 'bold'}, bbox_to_anchor=(0, -0.075, 1, 1))
+                for autotext in autotexts:
+                    autotext.set_weight('bold')
 
-            ax.axis('equal')
-            ax.set_title(labels[best_option_index], y=1, weight='bold', fontsize=40, color=colors[best_option_index])
+                # Draw circle
+                centre_circle = plt.Circle((0, 0), 0.65, fc='white')
+                fig = plt.gcf()
+                fig.gca().add_artist(centre_circle)
+                ax.annotate(sum(investment_counts), xy=(0, 0), fontsize=50, weight='bold', ha="center")
+                ax.annotate("Reviews", xy=(0, -0.25), fontsize=25, ha="center")
 
-            graph_placeholder.pyplot(fig)
+                # Draw bottom legend
+                labels_counts = [str(count) + " " + label for count, label in zip(list(investment_counts), labels)]
+                ax.legend(wedges, labels_counts, loc="lower center", ncol=3,
+                          prop={'weight': 'bold'}, bbox_to_anchor=(0, -0.075, 1, 1))
 
-            v_spacer(height=1)
+                ax.axis('equal')
+                ax.set_title(labels[best_option_index], y=1, weight='bold', fontsize=40, color=colors[best_option_index])
 
-            col_user_name, col_followers, col_tweet = tweets_placeholder.columns(3)
-            col_user_name.write("**User Name**")
-            col_followers.write("**Number of Followers**")
-            col_tweet.write("**Tweet**")
+                graph_placeholder.pyplot(fig)
 
-            # get best tweets
-            tweets_df = scraper.get_popular_tweets()
+                v_spacer(height=1)
 
-            for index, row in tweets_df.iterrows():
-                col_user_name.write(row['user'])
-                col_followers.write(row['followers_count'])
-                col_tweet.write(row['text'])
+                col_user_name, col_followers, col_tweet = tweets_placeholder.columns(3)
+                col_user_name.write("**User Name**")
+                col_followers.write("**Number of Followers**")
+                col_tweet.write("**Tweet**")
+
+                # get best tweets
+                tweets_df = scraper.get_popular_tweets()
+
+                for index, row in tweets_df.iterrows():
+                    col_user_name.write(row['user'])
+                    col_followers.write(row['followers_count'])
+                    col_tweet.write(row['text'])
+            else:
+                # st.title("No data found")
+                original_title = '<p style="color:Red; font-size: 30px; text-align: center">No data found</p>'
+                st.markdown(original_title, unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from twython import Twython
 import pandas as pd
 from datetime import timedelta, date
 from functools import reduce
+import random
 
 pd.set_option('display.max_colwidth', 5000)
 
@@ -16,12 +17,14 @@ class TwitterScraper:
         self.update_creds()
         self.python_tweets = Twython(self.credentials['CONSUMER_KEY'], self.credentials['CONSUMER_SECRET'],
                                      self.credentials['ACCESS_TOKEN'], self.credentials['ACCESS_SECRET'])
-        self.tokens_required = ["$amzn", "AMZN", "amazon", "#amazon", "amzn", "@amazon"]
-        # ["Facebook", "facebook", "meta", "fb", "#fb", "$fb"]
-        # ["Tsla", "tsla", "$tsla", "#tesla", "eilonmusk"]
-        # ["Ford", "ford", "f", "#f" ,"$f"]
+        self.tokens_required = {"amazon": ["$amzn", "AMZN", "amazon", "#amazon", "amzn", "@amazon"],
+                                "facebook": ["Facebook", "facebook", "meta", "fb", "#fb", "$fb"],
+                                "tesla": ["Tsla", "tsla", "$tsla", "#tesla", "eilonmusk"],
+                                "ford": ["Ford", "ford", "f", "#f", "$f"]
+                                }
         self.keywords = {
-            'buy_words': ["outperform", "buy", "sector perform", "hot", "bulles", "overweight", "positive", "strong buy"],
+            'buy_words': ["outperform", "buy", "sector perform", "hot", "bulles", "overweight", "positive",
+                          "strong buy"],
             'sell_words': ["sell", "underperform", "underweight", "underwt", "in-line", "frozen", "bleeding", "reduce"],
             'holding_words': ["hold", "neutral", "market perform"]}
         self.add_keywords()
@@ -34,21 +37,35 @@ class TwitterScraper:
         self.credentials['ACCESS_TOKEN'] = '1516112252390330372-w4HYVMnK0lBKmSzcInJOPbJ70NQ0M7'
         self.credentials['ACCESS_SECRET'] = 'lwYUTdFppmBh3E1paIVpdg8fmiApo0HwCgSrMeGAOlV5c'
 
-    def fetch_data(self, startDate, endDate):
+    def fetch_data(self, stock_name, startDate, endDate):
+        # init
+        self.df = pd.DataFrame({'user': [], 'followers_count': [], 'text': []})
+
         all_keywords = self.keywords['buy_words'] + self.keywords['sell_words'] + self.keywords['holding_words']
 
-        COUNT_TWEETS__FETCHED = 20  # TODO: change it to 2000
+        COUNT_TWEETS__FETCHED = 1000  # TODO: change it to 2000
+        total = 0
+        itr_idx = 0
         TWEETS_PER_REQUEST = 100
-        MAX_ATTEMPTS = int((COUNT_TWEETS__FETCHED / TWEETS_PER_REQUEST) * 2)
+        # MAX_ATTEMPTS = int((COUNT_TWEETS__FETCHED / TWEETS_PER_REQUEST) * 2)
+
         exceeding_max_amount = False
         LANG = 'en'
         data = {'user': [], 'date': [], 'text': [], 'favorite_count': [], 'followers_count': []}
+
+        # check if stock name exists
+        if stock_name not in self.tokens_required.keys():
+            return data
+
+        # shuffle tokens
+        tokens = self.tokens_required[stock_name]
+        random.shuffle(tokens)
 
         start_date = startDate
         end_date = endDate
 
         delta = timedelta(days=7)
-        for token in self.tokens_required:
+        for token in tokens:
 
             if exceeding_max_amount:
                 break
@@ -64,9 +81,11 @@ class TwitterScraper:
                 # for i in range(MAX_ATTEMPTS):
                 while week_bound:
 
-                    if COUNT_TWEETS__FETCHED <= len(data['user']):
+                    itr_idx += 1
+
+                    if COUNT_TWEETS__FETCHED <= total:
                         exceeding_max_amount = True
-                        print(f"Fetched {len(data['user'])} tweets at date {since}")
+                        print(f"Fetched {total} tweets at date {since}")
                         break
 
                     query = {'q': token,
@@ -85,19 +104,24 @@ class TwitterScraper:
                         results = self.python_tweets.search(**query, max_id=next_max_id)
 
                     first_itr = False
-                    print(f"loaded {len(results['statuses'])} tweets")
+
+                    total += len(results['statuses'])
 
                     # Save the returned tweets
                     for status in results['statuses']:
 
                         # save only at least one keyword exists in tweet
-                        if any(term in status['text'] for term in all_keywords):
-                            # print(status['text'])
+                        if occurences_counter(all_keywords, status['text']) > 0:
+                            # if any(term in status['text'] for term in all_keywords):
+
                             data['user'].append(status['user']['screen_name'])
                             data['date'].append(status['created_at'])
                             data['text'].append(status['text'])
                             data['favorite_count'].append(status['favorite_count'])
                             data['followers_count'].append(status['user']['followers_count'])
+
+                    # print stats
+                    print(f"({itr_idx}) found {len(data['user'])} out of {total} tweets loaded")
 
                     # Get the next max_id
                     try:
@@ -106,7 +130,7 @@ class TwitterScraper:
                     except Exception as e:
                         week_bound = False
 
-        self.df = pd.DataFrame(data)
+        self.df = pd.DataFrame(data)[['user', 'followers_count', 'text']]
         return data
 
     def add_keywords(self):
@@ -122,6 +146,10 @@ class TwitterScraper:
 
     def keywords_counter(self):
 
+        # check if dataframe is empty
+        if self.df.empty:
+            return 0, 0, 0
+
         sell_words = self.df.apply(lambda row: occurences_counter(self.keywords['sell_words'], row['text']), axis=1)
         buy_words = self.df.apply(lambda row: occurences_counter(self.keywords['buy_words'], row['text']), axis=1)
         hold_words = self.df.apply(lambda row: occurences_counter(self.keywords['holding_words'], row['text']), axis=1)
@@ -133,4 +161,5 @@ class TwitterScraper:
 
 
 # scraper = TwitterScraper()
-# scraper.fetch_data(date(2022, 4, 1), date(2022, 4, 20))
+# d = scraper.fetch_data("tesla", date(2022, 4, 1), date(2022, 4, 20))
+# print(scraper.keywords_counter())
